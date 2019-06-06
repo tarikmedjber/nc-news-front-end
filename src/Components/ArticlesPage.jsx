db838ba707947f9a7d3ff2eec6004df09cc65e8c
Tarik Medjber <tarikmedjber@Tariks-MacBook-Pro.local>
2019-06-06 17:17:54 +0100

    created drop down to sort articles

--- src/Components/ArticlesPage.jsx
+++ src/Components/ArticlesPage.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import ArticleList from "./ArticleList";
 
-import { getArticles, updateArticleVotes } from "../Api";
+import { getArticles } from "../Api";
 export default class ArticlesPage extends Component {
   state = {
-    articles: []
+    articles: [],
+    sortBy: "created_at"
   };
   componentDidMount() {
     getArticles().then(articles => {
       this.setState({ articles: articles });
     });
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.sortBy !== this.state.sortBy) {
+      getArticles({ sort_by: this.state.sortBy }).then(articles => {
+        this.setState({ articles: articles });
+      });
+    }
   }
 
   render() {
@@ -18,16 +26,12 @@ export default class ArticlesPage extends Component {
     return (
       <div>
         <h2>Articles</h2>
-        <button onClick={() => this.sortBy("created_at")}>
-          Filter by date created
-        </button>
-        <button onClick={() => this.sortBy("comment_count")}>
-          Filter by comment count
-        </button>
-        <button onClick={() => this.sortBy("votes")}>
-          Filter by vote count
-        </button>
-
+        Sort By:
+        <select onChange={this.sortBy} value={this.state.sortBy}>
+          <option value="created_at">Created At </option>
+          <option value="comment_count">Comment Count</option>
+          <option value="votes">Vote Count</option>
+        </select>
         <ul id="Article">
           <ArticleList
             handleVoteChange={this.handleVoteChange}
@@ -38,23 +42,7 @@ export default class ArticlesPage extends Component {
     );
   }
 
-  sortBy(searchTerm) {
-    if (this.state.orderBy === "desc") {
-      getArticles({ sort_by: searchTerm }).then(articles => {
-        this.setState({ articles: articles });
-      });
-    } else
-      getArticles({ sort_by: searchTerm, order: this.state.orderBy }).then(
-        articles => {
-          this.setState({ articles: articles });
-        }
-      );
-  }
-  // handleVoteChange = event => {
-  //   event.preventDefault();
-  //   let direction = { votes: 1 };
-  //   updateArticleVotes(article.article_id, direction).then(res => {
-  //     return res;
-  //   });
-  // };
+  sortBy = event => {
+    this.setState({ sortBy: event.target.value });
+  };
 }
