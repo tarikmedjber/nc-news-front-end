fe3f40a6388035265c4e05b887eaa0097d9ae3ec
Tarik Medjber <tarikmedjber@Tariks-MacBook-Pro.local>
2019-06-06 17:34:31 +0100

    fixed link issues in the homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { getArticles } from "../Api";
-import ArticleList from "./ArticleList";
+import { Link } from "@reach/router";
+
 import "./articles.css";
 
 export default class Homepage extends Component {
@@ -11,11 +12,30 @@ export default class Homepage extends Component {
     });
   }
   render() {
+    const { articles } = this.state;
     return (
       <div>
         <h2>Todays Top Two!</h2>
         <ul id="Article">
-          <ArticleList articles={this.state.articles} />
+          {articles.map(article => {
+            return (
+              <li key={article.article_id}>
+                <Link to={`/articles/${article.article_id}`}>
+                  <h3>{article.title}</h3>
+                </Link>
+
+                <h4>{`Created by ${article.author}  `}</h4>
+
+                <p>{`${article.votes} votes`}</p>
+
+                <Link to={`/articles/${article.article_id}/comments`}>
+                  {`${article.comment_count} comments`}
+                </Link>
+
+                <p id="CreatedAt">{article.created_at}</p>
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
