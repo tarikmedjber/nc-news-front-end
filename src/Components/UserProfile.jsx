adb5a6b0b78894eb23f3762713e11a634c73300f
Tarik Medjber <tarikmedjber@tariks-mbp.home>
2019-06-10 07:30:42 +0100

    started creating user profile page

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import { getUser, getArticles } from "../Api";
+export default class UserProfile extends Component {
+  state = { user: null };
 
-export default function UserArticle() {
-  return (
-    <div>
-      <h2>Users</h2>
-    </div>
-  );
+  componentDidMount() {
+    getUser(this.props.username).then(user => {
+      this.setState({ user: user });
+    });
+    getArticles({ user: this.props.username });
+  }
+  render() {
+    const { user } = this.state;
+
+    return (
+      user && (
+        <div>
+          <h2>{`${user.username}`}</h2>
+          <img src={user.avatar_url} alt="user avatar" />
+          <h4>{`${user.username}s Articles`}</h4>
+        </div>
+      )
+    );
+  }
 }
